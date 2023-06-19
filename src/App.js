e0cfed30e4f5653cefb5338390e8047f29fe0a2c
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2023-06-19 12:04:39 +0545

    class component

--- src/App.js
+++ src/App.js
@@ -1,74 +1,57 @@
-function App() {
-  const upload = () => {
-    var files = document.getElementById("file_upload").files;
-    if (files.length == 0) {
-      alert("Please choose any file...");
-      return;
-    }
-    var filename = files[0].name;
-    var extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
-    if (extension == ".CSV") {
-      //Here calling another method to read CSV file into json
-      csvFileToJSON(files[0]);
-    } else {
-      alert("Please select a valid csv file.");
-    }
+import React from 'react';
+
+class CSVtoJSONConverter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jsonData: null,
+    };
+  }
+
+  handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const text = e.target.result;
+      const jsonData = this.csvToJSON(text);
+      this.setState({ jsonData });
+    };
+
+    reader.readAsText(file);
   };
 
-  //Method to read csv file and convert it into JSON
-  function csvFileToJSON(file) {
-    try {
-      var reader = new FileReader();
-      reader.readAsBinaryString(file);
-      reader.onload = function (e) {
-        var jsonData = [];
-        var headers = [];
-        var rows = e.target.result.split("\r\n");
-        for (var i = 0; i < rows.length; i++) {
-          var cells = rows[i].split(",");
-          var rowData = {};
-          for (var j = 0; j < cells.length; j++) {
-            if (i == 0) {
-              var headerName = cells[j].trim();
-              headers.push(headerName);
-            } else {
-              var key = headers[j];
-              if (key) {
-                rowData[key] = cells[j].trim();
-              }
-            }
-          }
-          //skip the first row (header) data
-          if (i != 0) {
-            jsonData.push(rowData);
-          }
-        }
+  csvToJSON = (csvText) => {
+    const lines = csvText.split('\n');
+    const headers = lines[0].split(',');
 
-        //displaying the json result in string format
-        document.getElementById("display_data_in_readable_format").value =
-          JSON.stringify(jsonData, null, 4);
-      };
-    } catch (e) {
-      console.error(e);
+    const jsonData = [];
+    for (let i = 1; i < lines.length; i++) {
+      const currentLine = lines[i].split(',');
+      if (currentLine.length !== headers.length) continue;
+
+      const entry = {};
+      for (let j = 0; j < headers.length; j++) {
+        entry[headers[j]] = currentLine[j];
+      }
+      jsonData.push(entry);
     }
+
+    return jsonData;
+  };
+
+  render() {
+    const { jsonData } = this.state;
+
+    return (
+      <div>
+        <input type="file" onChange={this.handleFileUpload} />
+        {jsonData && (
+          <pre>{JSON.stringify(jsonData, null, 2)}</pre>
+        )}
+      </div>
+    );
   }
-  return (
-    <>
-      <center>
-        <h2>Upload a CSV file to convert in human readable JSON</h2>
-        {/* <!-- Input element to upload an csv file --> */}
-        <input type="file" id="file_upload" />
-        <button onClick={upload()}>Upload</button>
-        <br />
-        <br />
-        {/* <!-- container to display the csv data --> */}
-        <textarea
-          id="display_data_in_readable_format"
-          style={{height:"400px", width:"400px;"}}
-        ></textarea>
-      </center>
-    </>
-  );
 }
 
-export default App;
+export default CSVtoJSONConverter;
