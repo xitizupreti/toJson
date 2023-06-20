5e09c3b4b77824a1fa2b6e4e88b0c4e28ebab677
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2023-06-20 14:41:31 +0545

    functional

--- src/App.js
+++ src/App.js
@@ -19,10 +19,13 @@ const CSVtoJSONConverter = () => {
   const csvToJSON = (csvText) => {
     const lines = csvText.split('\n');
     const headers = lines[0].split(',');
-
+// console.log(lines);
+// console.log(headers.length);
     const jsonData = [];
     for (let i = 1; i < lines.length; i++) {
       const currentLine = lines[i].split(',');
+      // console.log(currentLine);
+      // console.log(currentLine.length);
       if (currentLine.length !== headers.length) continue;
 
       const entry = {};
@@ -30,6 +33,7 @@ const CSVtoJSONConverter = () => {
         entry[headers[j]] = currentLine[j];
       }
       jsonData.push(entry);
+      console.log(entry);
     }
 
     return jsonData;
