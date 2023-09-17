ab6aa95c5b6b4526f132ea826036be87fd0bfb28
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2023-09-17 09:21:46 +0545

    CSVTOJSON

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const CSVtoJSONConverter = () => {
   const [jsonData, setJsonData] = useState(null);
@@ -17,23 +17,33 @@ const CSVtoJSONConverter = () => {
   };
 
   const csvToJSON = (csvText) => {
-    const lines = csvText.split('\n');
-    const headers = lines[0].split(',');
-// console.log(lines);
-// console.log(headers.length);
+    const lines = csvText.split("\n");
+    const headers = lines[0].split(",");
+    // console.log(lines);
+    // console.log(headers.length);
     const jsonData = [];
     for (let i = 1; i < lines.length; i++) {
-      const currentLine = lines[i].split(',');
+      const currentLine = lines[i].split(",");
       // console.log(currentLine);
       // console.log(currentLine.length);
       if (currentLine.length !== headers.length) continue;
 
       const entry = {};
       for (let j = 0; j < headers.length; j++) {
+        // console.log(headers[j].replace("/STATION_ID/g", "origin_code"));
+        if (headers[j] === "STATION_ID") {
+          headers[j] = "origin_code";
+        } else if (headers[j] === "WATER_LEVEL (m)") {
+          headers[j] = "Value";
+        } else if (headers[j] === "STATION_NAME") {
+          continue;
+        } else if (headers[j] === "DISCHARGE (m3/s)") {
+          continue;
+        }
         entry[headers[j]] = currentLine[j];
       }
       jsonData.push(entry);
-      console.log(entry);
+      // console.log(entry);
     }
 
     return jsonData;
@@ -42,9 +52,7 @@ const CSVtoJSONConverter = () => {
   return (
     <div>
       <input type="file" onChange={handleFileUpload} />
-      {jsonData && (
-        <pre>{JSON.stringify(jsonData, null, 2)}</pre>
-      )}
+      {jsonData && <pre>{JSON.stringify(jsonData, null, 2)}</pre>}
     </div>
   );
 };
